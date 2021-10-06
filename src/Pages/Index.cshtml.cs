525474274a3b50778b167576a25729efa6e2f923
HaakenJ <kramerhjohnson@gmail.com>
2021-10-06 10:38:12 -0700

    Added name Kramer Johnson to IndexModel Comment in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Mike Koenig
+    /// Kramer Johnson
     /// </summary>
     public class IndexModel : PageModel
     {
