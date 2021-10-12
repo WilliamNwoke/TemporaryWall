f5324a758cbe28de39150e54e9f3bfda3c7039c4
HaakenJ <kramerhjohnson@gmail.com>
2021-10-11 19:37:56 -0700

    resolved conflict

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -3,12 +3,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-<<<<<<< HEAD
 
-// here is a change
-=======
 /// Henry Song - for GitHub lab excercise #3
->>>>>>> 4dc4f8b2afc6e945f3c53070faf420b0900e6eef
 namespace ContosoCrafts.WebSite.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
