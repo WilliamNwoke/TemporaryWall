0f095bb6dfe41c37b8f4ede41a1a8350f9a31f85
HaakenJ <kramerhjohnson@gmail.com>
2021-10-17 11:35:35 -0700

    Corrected route to light_mdoe.css file in site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -5,5 +5,5 @@ const theme = $("#theme-link");
 
 btn.on("click", () => {
     btn.prop("checked") ? theme.attr("href", "/css/dark_mode.css") :
-        theme.attr("href", "css/light_mode.css");
+        theme.attr("href", "/css/light_mode.css");
 })
