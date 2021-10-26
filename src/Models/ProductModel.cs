f20f6c5acac6b28cfe8fa658faf76a19e74ed3c9
Henry Song <henry.song671@gmail.com>
2021-10-26 08:46:24 -0700

    Added validation tags to Image, Url, Title, Description fields in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -9,10 +9,20 @@ namespace ContosoCrafts.WebSite.Models
         public string Id { get; set; }
         public string Maker { get; set; }
         
+        [Required]
+        [Url]
         [JsonPropertyName("img")]
         public string Image { get; set; }
+
+        [Required]
+        [Url]
         public string Url { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
+
+        [Required]
         public string Description { get; set; }
         public int[] Ratings { get; set; }
         public string[] Comments { get; set; }
