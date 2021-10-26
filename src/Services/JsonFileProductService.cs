8b0e7632e8faf30b3e41aaacbacbfcf5cd0b7599
Zhicong Zeng <zzeng1@seattleu.edu>
2021-10-26 10:53:05 -0700

    Reload the Michael's JsonFileProductService.

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -9,12 +9,8 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
 {
-   public class JsonFileProductService
+    public class JsonFileProductService
     {
-        /// <summary>
-        /// Initiated JsonFileProductService
-        /// </summary>
-        /// <param name="webHostEnvironment">environment</param>
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
@@ -22,21 +18,14 @@ namespace ContosoCrafts.WebSite.Services
 
         public IWebHostEnvironment WebHostEnvironment { get; }
 
-        /// <summary>
-        /// Returns file path of product json database
-        /// </summary>
         private string JsonFileName
         {
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
 
-        /// <summary>
-        /// Retrieves products
-        /// </summary>
-        /// <returns>iterable list of products</returns>
-        public IEnumerable<ProductModel> GetProducts()
+        public IEnumerable<ProductModel> GetAllData()
         {
-            using var jsonFileReader = File.OpenText(JsonFileName);//Use simple 'using' statement (IDE0063)
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
@@ -47,28 +36,58 @@ namespace ContosoCrafts.WebSite.Services
         }
 
         /// <summary>
-        /// Adds a rating to the specified product ID
+        /// Add Rating
+        /// 
+        /// Take in the product ID and the rating
+        /// If the rating does not exist, add it
+        /// Save the update
         /// </summary>
-        /// <param name="productId">ID of specific product</param>
-        /// <param name="rating">Rating to be added</param>
-        public void AddRating(string productId, int rating)
+        /// <param name="productId"></param>
+        /// <param name="rating"></param>
+        public bool AddRating(string productId, int rating)
         {
-            var products = GetProducts();
-
-            if(products.First(x => x.Id == productId).Ratings == null)
+            // If the ProductID is invalid, return
+            if (string.IsNullOrEmpty(productId))
             {
-                //creates new list of ratings
-                products.First(x => x.Id == productId).Ratings = new int[] { rating };
-            }
-            else
-            {
-                //adds rating to existing ratings list
-                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
-                ratings.Add(rating);
-                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+                return false;
             }
 
+            var products = GetAllData();
+
+            // Look up the product, if it does not exist, return
+            var data = products.FirstOrDefault(x => x.Id.Equals(productId));
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Check Rating for boundries, do not allow ratings below 0
+            if (rating < 0)
+            {
+                return false;
+            }
+
+            // Check Rating for boundries, do not allow ratings above 5
+            if (rating > 5)
+            {
+                return false;
+            }
+
+            // Check to see if the rating exist, if there are none, then create the array
+            if (data.Ratings == null)
+            {
+                data.Ratings = new int[] { };
+            }
+
+            // Add the Rating to the Array
+            var ratings = data.Ratings.ToList();
+            ratings.Add(rating);
+            data.Ratings = ratings.ToArray();
+
+            // Save the data back to the data store
             SaveData(products);
+
+            return true;
         }
 
         /// <summary>
@@ -78,7 +97,7 @@ namespace ContosoCrafts.WebSite.Services
         /// <param name="comment">Comment to be added</param>
         public void AddComment(string productId, string comment)
         {
-            var products = GetProducts();
+            var products = GetAllData();
 
             if (products.First(x => x.Id == productId).Comments == null)
             {
@@ -97,12 +116,39 @@ namespace ContosoCrafts.WebSite.Services
         }
 
         /// <summary>
+        /// Find the data record
+        /// Update the fields
+        /// Save to the data store
+        /// </summary>
+        /// <param name="data"></param>
+        public ProductModel UpdateData(ProductModel data)
+        {
+            var products = GetAllData();
+            var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
+            if (productData == null)
+            {
+                return null;
+            }
+
+            // Update the data to the new passed in values
+            productData.Title = data.Title;
+            productData.Description = data.Description.Trim();
+            productData.Url = data.Url;
+            productData.Image = data.Image;
+
+
+            SaveData(products);
+
+            return productData;
+        }
+
+        /// <summary>
         /// Save All products data to storage
         /// </summary>
         private void SaveData(IEnumerable<ProductModel> products)
         {
 
-            using var outputStream = File.Create(JsonFileName);//Use simple 'using' statement (IDE0063)
+            using (var outputStream = File.Create(JsonFileName))
             {
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
@@ -116,31 +162,6 @@ namespace ContosoCrafts.WebSite.Services
         }
 
         /// <summary>
-        /// Find the data record
-        /// Update the fields
-        /// Save to the data store
-        /// </summary>
-        /// <param name="data"></param>
-        public ProductModel UpdateData(ProductModel data)
-        {
-            var products = GetProducts();
-            var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
-            if (productData == null)
-            {
-                return null;
-            }
-
-            productData.Title = data.Title;
-            productData.Description = data.Description;
-            productData.Url = data.Url;
-            productData.Image = data.Image;
-
-            SaveData(products);
-
-            return productData;
-        }
-
-        /// <summary>
         /// Create a new product using default values
         /// After create the user can update to set values
         /// </summary>
@@ -150,14 +171,14 @@ namespace ContosoCrafts.WebSite.Services
             var data = new ProductModel()
             {
                 Id = System.Guid.NewGuid().ToString(),
-                Title = "",
-                Description = "",
-                Url = "",
+                Title = "Enter Title",
+                Description = "Enter Description",
+                Url = "Enter URL",
                 Image = "",
             };
 
             // Get the current set, and append the new record to it becuase IEnumerable does not have Add
-            var dataSet = GetProducts();
+            var dataSet = GetAllData();
             dataSet = dataSet.Append(data);
 
             SaveData(dataSet);
@@ -172,14 +193,15 @@ namespace ContosoCrafts.WebSite.Services
         public ProductModel DeleteData(string id)
         {
             // Get the current set, and append the new record to it
-            var dataSet = GetProducts();
+            var dataSet = GetAllData();
             var data = dataSet.FirstOrDefault(m => m.Id.Equals(id));
 
-            var newDataSet = GetProducts().Where(m => m.Id.Equals(id) == false);
+            var newDataSet = GetAllData().Where(m => m.Id.Equals(id) == false);
 
             SaveData(newDataSet);
 
             return data;
         }
+
     }
 }
