a6af42eea90f567d61896b5fa6067964568e6306
Zhicong Zeng <zzeng1@seattleu.edu>
2021-10-26 20:29:55 -0700

    Read Page Tests.

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -34,5 +34,47 @@ namespace UnitTests.Pages.Product.Read
             Assert.AreEqual("The Starry Night x Vincent van Gogh", pageModel.Product.Title);
         }
         #endregion OnGet
+
+        /*
+ #region GetComment
+ [Test]
+ public void GetComment_Valid_Should_Pass()
+ {
+ // Arrange
+
+ // Act
+
+ // Assert
+     Assert.AreEqual(null, pageModel.Comment);
+ }
+ #endregion GetComment
+
+ #region SetComment
+ [Test]
+ public void SetComment_Valid_Should_Add()
+ {
+     // Arrange
+
+     // Act
+
+     // Assert
+     Assert.AreEqual(null, pageModel.Comment('dfds');
+ }
+ #endregion GetComment
+
+
+ #region OnPost
+ [Test]
+ public void OnPost_Valid_Should_Pass()
+ {
+     // Arrange
+
+     // Act
+     pageModel.OnPost();
+
+     // Assert
+     Assert.AreEqual(true, result);
+ }
+ #endregion OnPost*/
     }
 }
