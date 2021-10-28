d0dd751f079b3bb11f5a7e7f156aabcf1adf1a90
HaakenJ <kramerhjohnson@gmail.com>
2021-10-27 22:25:46 -0700

    Added 'Description' back into ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -18,6 +18,8 @@ namespace ContosoCrafts.WebSite.Models
         [StringLength(maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
+        [Required]
+        public string Description { get; set; }
         public int[] Ratings { get; set; }
         public string[] Comments { get; set; }
 
