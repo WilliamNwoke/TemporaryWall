4a83295d3e054da27af32438e9bbdc7b0b8024a7
will <willteey@gmail.com>
2021-11-01 16:10:04 -0700

    Added comments for "json file product service test.cs"

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -3,16 +3,19 @@ using System.Linq;
 
 namespace UnitTests.Pages.Product.AddRating
 {
+    /// <summary>
+    /// Json File Product service test
+    /// </summary>
     public class JsonFileProductServiceTests
     {
         #region TestSetup
-
+        // test initialize
         [SetUp]
         public void TestInitialize()
         {
         }
         #endregion TestSetup
-
+        // Test funciton for rating null items
         #region AddRating
         [Test]
         public void AddRating_InValid_Product_Null_Should_Return_False()
@@ -26,6 +29,7 @@ namespace UnitTests.Pages.Product.AddRating
             Assert.AreEqual(false, data);
         }
 
+        // Test function to checking rating of invalid item
         [Test]
         public void AddRating_Valid_Product_InValid_Should_Return_False()
         {
@@ -38,6 +42,7 @@ namespace UnitTests.Pages.Product.AddRating
             Assert.AreEqual(false, data);
         }
 
+        // zTest function to test when rating is less than 0 and return false
         [Test]
         public void AddRating_Valid_Product_Valid_AddRating_SmallThanZero_Should_Return_False_()
         {
@@ -50,7 +55,7 @@ namespace UnitTests.Pages.Product.AddRating
             // Assert
             Assert.AreEqual(false, result);
         }
-
+        // Test function for add rating to return false when rating is bigger than 5 and return false
         [Test]
         public void AddRating_Valid_Product_Valid_AddRating_BiggerThanFive_Should_Return_False()
         {
@@ -64,6 +69,7 @@ namespace UnitTests.Pages.Product.AddRating
             Assert.AreEqual(false, result);
         }
 
+        // function to test the rating when it is null
         [Test]
         public void AddRating_Valid_Product_Valid_AddRating_Valid_Result_ValveIsNull_Should_Return_True()
         {
@@ -77,6 +83,7 @@ namespace UnitTests.Pages.Product.AddRating
             Assert.AreEqual(true, result);
         }
 
+        // Function to test the add rating functionality
         [Test]
         public void AddRating_Valid_Product_Valid_Rating_Valid_Should_Return_True()
         {
@@ -101,16 +108,21 @@ namespace UnitTests.Pages.Product.AddRating
 
 namespace UnitTests.Pages.Product.AddComment
 {
+    /// <summary>
+    /// Json Product services test
+    /// </summary>
     public class JsonFileProductServiceTests
     {
         #region TestSetup
 
+        // test initialize
         [SetUp]
         public void TestInitialize()
         {
         }
         #endregion TestSetup
 
+        // function to test the add comment or pass
         #region AddComment
         [Test]
         public void AddComment_Valid_Data_Valid_Comment_Valid_AddComment_Should_Pass()
@@ -123,7 +135,7 @@ namespace UnitTests.Pages.Product.AddComment
 
         }
         #endregion AddComment
-
+        // Function to test the add comment
         #region AddComment
         [Test]
         public void AddComment_Valid__Data_Valid_Comment_Invalid_AddComment_Should_Pass()
@@ -138,7 +150,7 @@ namespace UnitTests.Pages.Product.AddComment
         }
         #endregion AddComment
 
-
+        // function to return product model as string
         #region ProductModeltoString
         [Test]
         public void ProductModeltoString_Valid_Should_Pass()
