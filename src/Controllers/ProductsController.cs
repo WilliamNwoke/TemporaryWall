af9b4f12c2d3ccd82283c1052f601bc7b7bcea6b
will <willteey@gmail.com>
2021-11-01 13:21:16 -0700

    Added comments for the products controller class and method, added comments for the commenting class and get set method

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -9,22 +9,28 @@ namespace ContosoCrafts.WebSite.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    /// <summmary>
+    /// Controller class for the products.json
+    /// </summmary>
     public class ProductsController : ControllerBase
-    {
+    {   
+        // Product service method
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
-
+        // Product service get function
         public JsonFileProductService ProductService { get; }
 
         [HttpGet]
+        //Product service http get function
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetProducts();
         }
 
         [HttpPatch]
+        // product service rating method
         public ActionResult Patch([FromBody] RatingRequest request)
         {
             ProductService.AddRating(request.ProductId, request.Rating);
@@ -32,6 +38,8 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+
+        // get set method for rating
         public class RatingRequest
         {
             public string ProductId { get; set; }
@@ -39,6 +47,7 @@ namespace ContosoCrafts.WebSite.Controllers
         }
 
         [HttpPatch]
+        // Product Add comment method
         public ActionResult Patch([FromBody] CommentRequest request)
         {
             ProductService.AddComment(request.ProductId, request.Comment);
@@ -46,9 +55,15 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// comment request class
+        /// </summary>
         public class CommentRequest
         {
+            // get set method for the product ID
             public string ProductId { get; set; }
+
+            // get set method for the comment
             public string Comment { get; set; }
         }
     }
