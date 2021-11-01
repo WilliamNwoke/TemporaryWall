c9e0f6df67549f284676c66373eda76ffe110b9e
will <willteey@gmail.com>
2021-11-01 14:15:30 -0700

    Added comments for the Test index file for the products

--- UnitTests/Pages/Product/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -7,15 +7,19 @@ using NUnit.Framework;
 using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Index
-{
+{   
+    /// <summary>
+    /// Unit test for the index functionality
+    /// </summary>
     public class IndexTests
-    {
+    {   
+        // Test to see the management of index file
         #region TestSetup
         public static PageContext pageContext;
-
         public static IndexModel pageModel;
 
         [SetUp]
+        // Test Initialize method
         public void TestInitialize()
         {
             pageModel = new IndexModel(TestHelper.ProductService)
@@ -24,7 +28,7 @@ namespace UnitTests.Pages.Product.Index
         }
 
         #endregion TestSetup
-
+        // onGet test to return a list of products
         #region OnGet
         [Test]
         public void OnGet_Valid_Should_Return_Products()
