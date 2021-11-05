6e57352b73d6a4b76334aff16b0ec3173e8939a5
Haakasaurus <kramerhjohnson@gmail.com>
2021-11-05 11:51:39 -0700

    Renamed Index.cshtml to Home.cshtml

---
+++ src/Pages/Home.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// Model for the Index Page
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        // Retrieves Product Service
+        public JsonFileProductService ProductService { get; }
+        // Retreive Products and privately sets 
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        // Retrieves Products using razor component
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
