4e56cea1773beba42c04de9bcb778a5afd2df0b3
Haakasaurus <kramerhjohnson@gmail.com>
2021-11-05 13:57:01 -0700

    Switched the values of checked vs unchecked for the dark mode switch in site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -4,6 +4,6 @@ const btn = $(".dark-mode-switch");
 const theme = $("#theme-link");
 
 btn.on("click", () => {
-    btn.prop("checked") ? theme.attr("href", "/css/dark_mode.css") :
-        theme.attr("href", "/css/light_mode.css");
+    btn.prop("checked") ? theme.attr("href", "/css/light_mode.css") :
+        theme.attr("href", "/css/dark_mode.css");
 })
