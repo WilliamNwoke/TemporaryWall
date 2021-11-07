3056b1e1a725fc5ce1c1d11e82c0c8a628de8d53
will <willteey@gmail.com>
2021-11-07 15:20:32 -0800

    Removed the unnecessary imports from Pages/Aboutus/MemberIndex.cshtml.cs

--- src/Pages/Aboutus/MemberIndex.cshtml.cs
+++ src/Pages/Aboutus/MemberIndex.cshtml.cs
@@ -1,9 +1,4 @@
-using System.Collections.Generic;
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Aboutus
 {
