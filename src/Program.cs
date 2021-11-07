aa13386582c7ce7092e9e7a63a4f39b88374ce5f
will <willteey@gmail.com>
2021-11-07 15:45:15 -0800

    Linebreaks standard for program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -12,11 +12,12 @@ namespace ContosoCrafts.WebSite
         /// <summary>
         /// main function of the class creates a host using the instance of the IHostBuilder
         /// </summary>
-        /// <param name="args"></param>
+        /// <param name = "args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
+
         // Iwebhosting method
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
