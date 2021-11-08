cf0d6d9b212f2817e2b8939a23962a44fa861754
Henry Song <henry.song671@gmail.com>
2021-11-08 11:55:05 -0800

    Initialized Tests in Landing.Tests.cs

--- UnitTests/Pages/Landing.Tests.cs
+++ UnitTests/Pages/Landing.Tests.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.AspNetCore.Mvc;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
+
+namespace UnitTests.Pages.Landing
+{
+    /// <summary>
+    /// Json Product services test
+    /// </summary>
+    public class LandingTests
+    {
+        #region TestSetup
+        public static LandingModel pageModel;
+
+        // test initialize
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new LandingModel(TestHelper.ProductService);
+        }
+        #endregion TestSetup
+
+
+        
+    }
+}
