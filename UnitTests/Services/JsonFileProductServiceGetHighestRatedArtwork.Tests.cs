f34d259427734458e48216e2c3c1f64dff81babe
Henry Song <henry.song671@gmail.com>
2021-11-08 11:49:29 -0800

    Added unittests for GetHighestRatedArtwork() in JsonFileProductServiceGetHighestRatedArtwork.Tests.cs

--- UnitTests/Services/JsonFileProductServiceGetHighestRatedArtwork.Tests.cs
+++ UnitTests/Services/JsonFileProductServiceGetHighestRatedArtwork.Tests.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests.Services.JsonFileProductService.GetHighestRatedArtwork
+{
+    /// <summary>
+    /// Json Product services test
+    /// </summary>
+    public class JsonFileProductServiceGetHighestRatedArtworkTest
+    {
+        #region TestSetup
+
+        // test initialize
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+        #endregion TestSetup
+
+
+        #region GetHighestRatedArtwork
+        /// <summary>
+        /// Checks if GetHighestRatedArtwork returns list of three entries.
+        /// Impossible to verify list's accuracy as ratings will change.
+        /// </summary>
+        [Test]
+        public void GetHighestRatedArtwork_Valid_Should_Return_List_of_3_True_Pass()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetHighestRatedArtwork();
+
+            // Act
+            var result = data.Count();
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        #endregion GetHighestRatedArtork
+    }
+}
