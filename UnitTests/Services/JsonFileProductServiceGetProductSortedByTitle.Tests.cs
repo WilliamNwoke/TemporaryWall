036520aa033fb0ab9d15828c80efd22528c0327d
Henry Song <henry.song671@gmail.com>
2021-11-15 12:35:25 -0800

    Added TestSetup in JsonFileProductServiceGerProductSortedByTitle.Tests.cs

--- UnitTests/Services/JsonFileProductServiceGetProductSortedByTitle.Tests.cs
+++ UnitTests/Services/JsonFileProductServiceGetProductSortedByTitle.Tests.cs
@@ -1,10 +1,25 @@
-﻿using System;
-namespace UnitTests.Services
+﻿using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace UnitTests.Services.JsonFileProductService.GetProductSortedByTitle
 {
+	/// <summary>
+	/// Json Product services test for sorting by title name
+	/// </summary>
 	public class JsonFileProductServiceGetProductSortedByTitle
 	{
-		public JsonFileProductServiceGetProductSortedByTitle()
-		{
-		}
-	}
+        #region TestSetup
+
+        /// <summary>
+        /// Initializes Tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+        #endregion TestSetup
+
+    }
 }
