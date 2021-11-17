8cc85fbaafab1380a4c8cf3b3ea52dd8bf236159
HaakenJ <kramerhjohnson@gmail.com>
2021-11-16 18:14:08 -0800

    Updated unit tests to test for parameter passing to OnGet and sorting in Index.cshtml.Tests.cs

--- UnitTests/Pages/Product/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -7,12 +7,12 @@ using NUnit.Framework;
 using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Index
-{   
+{
     /// <summary>
     /// Unit test for the index functionality
     /// </summary>
     public class IndexTests
-    {   
+    {
         // Test to see the management of index file
         #region TestSetup
         public static PageContext pageContext;
@@ -30,13 +30,13 @@ namespace UnitTests.Pages.Product.Index
 
         #region OnGet
         /// <summary>
-        /// onGet test to return a list of products
+        /// onGet test to return a list of products not sorted
         /// </summary>
         [Test]
         public void PageMode_OnGet_Valid_Assert_Should_Return_Data()
         {
             // Arrange
-            pageModel.OnGet();
+            pageModel.OnGet(null);
 
             // Act
             var data = pageModel.Products.FirstOrDefault();
@@ -44,6 +44,65 @@ namespace UnitTests.Pages.Product.Index
             // Assert
             Assert.IsNotNull(data);
         }
-        #endregion OnGet
+
+        /// <summary>
+        /// onGet test to return a list of products sorted by artist
+        /// </summary>
+        [Test]
+        public void PageMode_OnGet_Valid_Sort_By_Artist_Assert_Should_Return_Sorted_Data()
+        {
+            // Arrange
+            pageModel.OnGet("artist_asc");
+            var products = TestHelper.ProductService.GetProducts();
+
+            // Act
+            products = products.OrderBy(x => x.Artist);
+            var pageProducts = pageModel.Products;
+
+            // Assert
+            for (int i = 0; i < products.Count(); i++)
+            {
+                Assert.AreEqual(products.ElementAt(i).Artist, pageProducts.ElementAt(i).Artist);
+            }
+        }
+
+        /// <summary>
+        /// onGet test to return a list of products sorted by title
+        /// </summary>
+        [Test]
+        public void PageMode_OnGet_Valid_Sort_By_Title_Assert_Should_Return_Sorted_Data()
+        {
+            // Arrange
+            pageModel.OnGet("title_asc");
+            var products = TestHelper.ProductService.GetProducts();
+
+            // Act
+            products = products.OrderBy(x => x.Title);
+            var pageProducts = pageModel.Products;
+
+            // Assert
+            for (int i = 0; i < products.Count(); i++)
+            {
+                Assert.AreEqual(products.ElementAt(i).Title, pageProducts.ElementAt(i).Title);
+            }
+        }
+
+        /// <summary>
+        /// onGet test to return a list of products sorted by title
+        /// </summary>
+        [Test]
+        public void PageMode_OnGet_Valid_Sort_By_Rating_Assert_Should_Return_Sorted_Data()
+        {
+            // Arrange
+            pageModel.OnGet("rating_desc");
+            var products = TestHelper.ProductService.GetProducts();
+
+            // Act
+            var data = pageModel.Products.FirstOrDefault();
+
+            // Assert
+            Assert.IsNotNull(data);
+            #endregion OnGet
+        }
     }
 }
