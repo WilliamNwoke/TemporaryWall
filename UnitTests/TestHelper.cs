09f6b5b1fd1e277d32674fd05a38f488811ba0ce
Zhicong Zeng <zzeng1@seattleu.edu>
2021-11-28 10:48:23 -0800

    Add comments in TestHelper.cs

--- UnitTests/TestHelper.cs
+++ UnitTests/TestHelper.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,9 +6,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
-
 using Moq;
-
 using ContosoCrafts.WebSite.Services;
 using ContosoCrafts.WebSite.Controllers;
 
@@ -28,6 +25,7 @@ namespace UnitTests
     /// </summary>
     public static class TestHelper
     {
+        //Create static Default Constructor
         public static Mock<IWebHostEnvironment> MockWebHostEnvironment;
         public static IUrlHelperFactory UrlHelperFactory;
         public static DefaultHttpContext HttpContextDefault;
@@ -48,35 +46,44 @@ namespace UnitTests
         /// </summary>
         static TestHelper()
         {
+            // Initializes an instance of the mock with IWebHostEnvironment and set up the mocked type
             MockWebHostEnvironment = new Mock<IWebHostEnvironment>();
             MockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
             MockWebHostEnvironment.Setup(m => m.WebRootPath).Returns(TestFixture.DataWebRootPath);
             MockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns(TestFixture.DataContentRootPath);
 
+            // Initializes an instance of DefaultHttpContext and set up
             HttpContextDefault = new DefaultHttpContext()
             {
                 TraceIdentifier = "trace",
             };
             HttpContextDefault.HttpContext.TraceIdentifier = "trace";
 
+            // Initializes an instance of ModelStateDictionary
             ModelState = new ModelStateDictionary();
 
+            // Initializes an instance of ActionContext and set up
             ActionContext = new ActionContext(HttpContextDefault, HttpContextDefault.GetRouteData(), new PageActionDescriptor(), ModelState);
 
+            // Initializes an instance of EmptyModelMetadataProvider
             ModelMetadataProvider = new EmptyModelMetadataProvider();
+
+            // Initializes an instance of ViewDataDictionary and set up 
             ViewData = new ViewDataDictionary(ModelMetadataProvider, ModelState);
+
+            // Initializes an instance of TempDataDictionary and set up
             TempData = new TempDataDictionary(HttpContextDefault, Mock.Of<ITempDataProvider>());
 
+            // Initializes an instance of PageContext and set up 
             PageContext = new PageContext(ActionContext)
             {
                 ViewData = ViewData,
                 HttpContext = HttpContextDefault
             };
 
+            // Initializes JsonFileProductService and set up 
             ProductService = new JsonFileProductService(MockWebHostEnvironment.Object);
-
             JsonFileProductService productService;
-
             productService = new JsonFileProductService(TestHelper.MockWebHostEnvironment.Object);
 
             // Creates ProductsController for use in TestHelper
